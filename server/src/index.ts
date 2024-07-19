89264e514c68f5738d75fb2cc84d81943262fbee
Prasanna Raj Karki <63549402+prasannakarki77@users.noreply.github.com>
2024-07-19 22:09:59 +0545

    socket io server setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,27 @@
-console.log("project runss");
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import cors from "cors";
+
+const port = 8000;
+const app = express();
+app.use(cors);
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("User is connected");
+
+  socket.on("disconnect", () => {
+    console.log("user is disconnected");
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Listening to port ${port} `);
+});
